db80022a96af6c94014389b46586f1df95bf6a4b
root <root@titdsftsmw.local>
2026-02-22 20:03:16 +0000

    FIX: remove invalid declare statement causing TS error

--- apps/server/src/prisma/prisma.service.ts
+++ apps/server/src/prisma/prisma.service.ts
@@ -3,8 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
-  // Explicitly expose Prisma delegates for TypeScript
-  declare channelMember: PrismaClient['channelMember'];
   async onModuleInit() {
     await this.$connect();
   }
